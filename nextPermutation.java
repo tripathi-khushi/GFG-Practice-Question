4999b2094086e51277ca9737840027294329a76a
tripathi-khushi <tripathikhushi000@gmail.com>
2026-01-02 23:59:40 +0530

    nextPermutation

---
+++ nextPermutation.java
@@ -0,0 +1,41 @@
+public class nextPermutation {
+    
+    void nextpermutation(int[] arr) {
+        // code here
+        int i = arr.length - 2;
+        while(i>=0 && arr[i]>=arr[i+1]){
+            i--;
+        }
+        if(i>=0){
+            int j = arr.length-1;
+            while(arr[i]>=arr[j]){
+                j--;
+            }
+            
+            swap(arr,i,j);
+        }
+        
+        reverse(arr,i+1,arr.length-1);
+    }
+    
+     static void swap(int[] arr, int i, int j ){
+       
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        
+     }
+        
+        static void reverse(int[] arr, int i, int j){
+            while(i<j){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+    
+}
+    
+
