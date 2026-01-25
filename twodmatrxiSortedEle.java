f9b8ef998bece822d43e534649c2fa00675e6165
tripathi-khushi <tripathikhushi000@gmail.com>
2026-01-25 12:40:12 +0530

    2d matrix with sorted element find target value

---
+++ twodmatrxiSortedEle.java
@@ -0,0 +1,28 @@
+public class twodmatrxiSortedEle {
+     public boolean searchMatrix(int[][] matrix, int target) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int low = 0;
+        int high = m*n-1;
+
+        while(low<=high){
+            int mid = (low+high)/2;
+            int row = mid / n;
+            int column = mid % n;
+            if(matrix[row][column]==target){
+                return true;
+            }
+            else if(matrix[row][column]>target){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        } 
+
+        return false;
+    }    
+    
+}
