f5775c1083db6e3798bb79efdfe86408585e6958
tripathi-khushi <tripathikhushi000@gmail.com>
2026-01-27 10:19:39 +0530

    reaarange array element by sign

---
+++ rearrangeArrayElementBySign.java
@@ -0,0 +1,22 @@
+public class rearrangeArrayElementBySign {
+    
+    public int[] rearrangeArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n]; 
+        int pos = 0;
+        int neg = 1;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] >= 0) {
+                result[pos] = nums[i]; 
+                pos += 2;
+            } else {
+                result[neg] = nums[i]; 
+                neg += 2;
+            }
+        }
+
+        return result; 
+    }
+
+}
