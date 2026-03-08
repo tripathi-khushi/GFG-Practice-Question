3454bd95ee98ac66bdb08bb778be5bf36e84f79d
tripathi-khushi <tripathikhushi000@gmail.com>
2026-03-08 12:03:59 +0530

    Count Primes

---
+++ countPrimes.java
@@ -0,0 +1,31 @@
+class countPrimes {
+    public int countPrimess(int n) {
+        
+        if(n<=2){
+            return 0;
+        }
+
+        boolean[] isPrime = new boolean[n];
+        for(int i = 2;  i < n; i++){
+           isPrime[i] = true;
+        }
+
+        for(int i = 2; i*i < n; i ++){
+            if(isPrime[i]){
+                for(int j = i*i; j < n; j+=i){
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        int count = 0;
+
+        for(int i = 2; i < n; i++){
+            if(isPrime[i]){
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
